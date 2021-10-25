af551152dc3cb102ba4bfc3a3ec2154a3a0fec87
Ben <benboorstein@gmail.com>
2021-10-25 09:08:34 -0400

    initial commit for Frontend Mentor project nine

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/*
+- use Grid
+- use the JSON file they provide
+*/
+
+/*
+Your users should be able to:
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+*/
