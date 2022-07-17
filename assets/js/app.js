a699eb3f5b694c0877db9acb7e83763fd22889af
Ben <benboorstein@gmail.com>
2022-07-17 10:10:17 -0400

    Minor changes. Project finished for now.

--- assets/js/app.js
+++ assets/js/app.js
@@ -113,6 +113,7 @@ let tracker = 'daily' // 'tracker' is just STARTING OFF as 'daily'
 
 // calling this function here so that on load of page 'daily' data is shown (this works because 'tracker' is set to 'daily' and it hasn't been updated yet) 
 updateCards()
+// Robert: "This is a great way to get the job done. It also is what you'd do when using something like Vue: You would likely call a function in Vue's purpose-built 'mounted' or 'created' method in order to initialize everything. So, in essence, you are calling 'updateCards()' once the page loads...in order to refresh the data to reflect your tracker being set to 'daily'. Great work!"
 
 document.querySelectorAll('.time-period-btn').forEach(button => {
   button.addEventListener('click', function() {
@@ -124,7 +125,7 @@ document.querySelectorAll('.time-period-btn').forEach(button => {
 
 function updateCards() {
   document.querySelectorAll('.card').forEach(card => {
-    let match = data.find(obj => obj.title === card.dataset.label) // ref notes under code for explanation of 'card.dataset.label' snippet
+    let match = data.find(obj => obj.title === card.dataset.label) // ref notes under code for explanation of 'card.dataset.label'
     card.querySelector('.hours-current').textContent = match.timeframes[tracker].current
     card.querySelector('.hours-previous').textContent = match.timeframes[tracker].previous
     document.querySelectorAll('.previous-period').forEach(period => {
@@ -137,7 +138,7 @@ function updateCards() {
 
 1) Note that 'element.dataset.keyname' -- applied in the line above as 'card.dataset.label' -- is the standard way of accessing the value(s) stored in 'keyname' ('label'). 'label' (just the suffix Robert and I chose for the HTML 'data-' attribute) is the KEY in the key-value pair, and the HTML attribute value (e.g., "Work", "Play", etc.) is the VALUE in the key-value pair. Look to the code examples below to see how logging these key-value pairs looks. Each looks like an object but is actually a DOMStringMap.
 
-2) 'dataset' is a property on the HTML element (which is represented by 'card' in this case). In Robert: 'dataset' isn't a global object that aggregates every element's 'data-' attributes, but rather is a property on any HTML element that has 'data-' attributes.
+2) 'dataset' is a property on the HTML element (which is represented by 'card' in this case). Robert: 'dataset' isn't a global object that aggregates every element's 'data-' attributes, but rather is a property on any HTML element that has 'data-' attributes.
 
 3) These are the relevant MDNs: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset and https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes.
 
